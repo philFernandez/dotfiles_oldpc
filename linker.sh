089d660082ee48d88ee424ced71e744e9b22873f
philFernandez <norcal618@live.com>
2018-06-29 10:22:58 -0700

    debug linker.sh

--- linker.sh
+++ linker.sh
@@ -6,17 +6,33 @@ VIMFUNDIR=$HOME/.vim/plugin/
 ZSHDIR=$HOME/.oh-my-zsh/custom/
 I3DIR=$HOME/.config/i3/
 
-mkdir -p $VIMFUNDIR && ln -s ./functions.vim $VIMFUNDIR/functions.vim
-mkdir -p $ZSHDIR && ln -s ./global_aliases.zsh $ZSHDIR/global_aliases.zsh
-mkdir -p $I3DIR && ln -s ./i3.config $I3DIR/config
-ln -s ./i3status.conf $HOME/.i3status.conf
-ln -s ./myFunctions.zsh $ZSHDIR/myFunctions.zsh
-ln -s ./Z_aliases.zsh $ZSHDIR/Z_aliases.zsh
-ln -s ./.tern-config $HOME/.tern-config
-ln -s ./.tmux.conf $HOME/.tmux.conf
-ln -s ./.vimrc $HOME/.vimrc
-ln -s ./.zshenv $HOME/.zshenv
-ln -s ./.zshrc $HOME/.zshrc
+#for file in $(ls -A | grep -Ev 'linker.sh|.git|README.md'); do
+  #chmod 664 $file
+#done
+
+(ls -A $HOME/.zshrc >> /dev/null 2>&1 && mv $HOME/.zshrc $HOME/.zshrc.bak)
+(ls -A $HOME/.vimrc >> /dev/null 2>&1 && mv $HOME/.vimrc $HOME/.vimrc.bak)
+(ls -A $I3DIR/config >> /dev/null 2>&1 && mv $I3DIR/config $I3DIR/config.bak)
+(ls -A $HOME/.tern-config >> /dev/null 2>&1 && mv $HOME/.tern-config $HOME/.tern-config.bak)
+(ls -A $HOME/.tmux.conf >> /dev/null 2>&1 && mv $HOME/.tmux.conf $HOME/.tmux.conf.bak)
+(ls -A $HOME/.zshenv >> /dev/null 2>&1 && mv $HOME/.zshenv $HOME/.zshenv.bak)
+(ls -A $VIMFUNDIR/functions.vim >> /dev/null 2>&1 && mv $VIMFUNDIR/functions.vim $VIMFUNDIR/functions.vim.bak)
+(ls -A $ZSHDIR/global_aliases.zsh >> /dev/null 2>&1 && mv $ZSHDIR/global_aliases.zsh $ZSHDIR/global_aliases.zsh.bak)
+(ls -A $ZSHDIR/myFunctions.zsh >> /dev/null 2>&1 && mv $ZSHDIR/myFunctions.zsh $ZSHDIR/myFunctions.zsh.bak)
+(ls -A $ZSHDIR/Z_aliases.zsh >> /dev/null 2>&1 && mv $ZSHDIR/Z_aliases.zsh $ZSHDIR/Z_aliases.zsh.bak)
+(ls -A $HOME/.i3status.conf >> /dev/null 2>&1 && mv $HOME/.i3status.conf $HOME/.i3status.conf.bak)
+
+mkdir -p $VIMFUNDIR && ln -s $HOME/.dotfiles/functions.vim $VIMFUNDIR/functions.vim
+mkdir -p $ZSHDIR && ln -s $HOME/.dotfiles/global_aliases.zsh $ZSHDIR/global_aliases.zsh
+mkdir -p $I3DIR && ln -s $HOME/.dotfiles/i3.config $I3DIR/config
+ln -s $HOME/.dotfiles/i3status.conf $HOME/.i3status.conf
+ln -s $HOME/.dotfiles/myFunctions.zsh $ZSHDIR/myFunctions.zsh
+ln -s $HOME/.dotfiles/Z_aliases.zsh $ZSHDIR/Z_aliases.zsh
+ln -s $HOME/.dotfiles/.tern-config $HOME/.tern-config
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.zshenv $HOME/.zshenv
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 
 
